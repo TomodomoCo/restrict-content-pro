7ac78c264e762fb04cd522b3e9fbfdcd2af030e6
Chris Van Patten <chris@vanpattenmedia.com>
2018-07-25 18:17:54 -0400

    Add a timestamp that the system info is generated; fixes #1862

--- includes/admin/tools/system-info.php
+++ includes/admin/tools/system-info.php
@@ -349,7 +349,10 @@ function rcp_tools_system_info_report() {
 
 	$return = apply_filters( 'rcp_system_info_after_php_extensions', $return );
 
-	$return .= "\n" . '### End System Info ###';
+	$return .= "\n" . '-- Miscellaneous' . "\n\n";
+	$return .= 'System Info Generated:    ' . date( 'Y-m-d H:i:s', current_time( 'timestamp' ) );
+
+	$return .= "\n\n" . '### End System Info ###';
 
 	return $return;
 }
